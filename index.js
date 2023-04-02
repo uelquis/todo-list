008fb722fdfc6addbf412e703f1f226685c468ca
Emanuel Marquis <eman0dev@gmail.com>
2023-04-02 11:50:27 -0300

    feat:added checkboxes

--- index.js
+++ index.js
@@ -1,18 +1,67 @@
 
 const form = document.getElementById("task-creator");
-const taskInputField = document.getElementById("task-input");
+const taskCreatorInputField = document.getElementById("task-input");
 const todoList = document.getElementById("todo-list");
 
 form.addEventListener("submit", function (event) {
-    const listItem = document.createElement("li")
-    listItem.textContent = taskInputField.value;
+    event.preventDefault();
 
-    if(listItem.textContent == "") {
-        event.preventDefault();
-        return;
+    const task = createElement("span").appendChildren(
+        createElement("input")
+            .setInputType("checkbox")
+            .setName("checkbox")
+            .getInstance(),
+        createElement("label")
+            .setTextContent(taskCreatorInputField.value)
+            .assignLabelTo("checkbox")
+            .getInstance()
+    );
+
+    const listItem = createElement("li")
+        .setClassName("task")
+        .appendChild(task.getInstance());
+    
+    if(listItem.getTextContent() == "") return;
+
+    taskCreatorInputField.value = ""; // clear input field
+    todoList.appendChild(listItem.getInstance());
+});
+
+function createElement(htmlElement) {
+    const element = document.createElement(htmlElement);
+    document.getC
+    const self = {
+        getInstance: () => element,
+        getTextContent: () => element.textContent,
+        setTextContent: (textContent) => {
+            element.textContent = textContent; 
+            return self;
+        },
+        setInputType: (inputType) => {
+            element.type = inputType;
+            return self;
+        },
+        setClassName: (className) => {
+            element.className = className;
+            return self;
+        },
+        setName: (name) => {
+            element.name = name;
+            return self;
+        },
+        assignLabelTo: (elementName) => {
+            element.htmlFor = elementName;
+            return self;
+        },
+        appendChild: (child) => {
+            element.appendChild(child);
+            return self;
+        },
+        appendChildren: (...children) => {
+            element.append(...children);
+            return self;
+        }
     }
 
-    taskInputField.value = "";
-    todoList.appendChild(listItem);
-    event.preventDefault();
-});
+    return self;
+}
